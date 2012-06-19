7309ced78e4fe3fc85d3a4564f2ea8528371e3ba
non <mail@non117.com>
2012-06-19 20:14:21 +0900

    _(:3」∠)_ commitし忘れてた

--- src/lib/outputs/imkayac.py
+++ src/lib/outputs/imkayac.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import hashlib
 import urllib, urllib2
+from Queue import Queue
 
 from lib.core import Output
 
 class imkayac(Output):
+    def init(self):
+        self.q = Queue()
+    
     def throw(self, packet):
         message = packet["data"]
         url = "http://im.kayac.com/api/post/%s" % self.username
@@ -17,15 +21,18 @@ class imkayac(Output):
         if not self.sendable(message):
             return None
         
-        params = {"message":message}
-        if hasattr(self, "password"):
-            params["password"] = self.password
-        if hasattr(self, "sig"):
-            params["sig"] = hashlib.sha1("%s%s" % (message, self.sig)).hexdigest()
+        self.q.put(message)
         
-        request = urllib2.Request(url, data=urllib.urlencode(params))
-        try:
-            response = urllib2.urlopen(request).read()
-        except urllib2.URLError:
-            pass
-        return "posted" in response
+        while not self.q.empty():
+            message = self.q.get()
+            params = {"message":message}
+            if hasattr(self, "password"):
+                params["password"] = self.password
+            if hasattr(self, "sig"):
+                params["sig"] = hashlib.sha1("%s%s" % (message, self.sig)).hexdigest()
+            
+            request = urllib2.Request(url, data=urllib.urlencode(params))
+            try:
+                urllib2.urlopen(request).read()
+            except urllib2.URLError:
+                self.q.put(message)
